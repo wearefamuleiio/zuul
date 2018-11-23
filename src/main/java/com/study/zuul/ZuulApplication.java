33aa95451ca47c4d7063f76e80c102eb3b4cccbb
王智鹏 <799208766@qq.com>
2018-11-23 15:26:22 +0800

    将zuul添加到eureka

--- src/main/java/com/study/zuul/ZuulApplication.java
+++ src/main/java/com/study/zuul/ZuulApplication.java
@@ -2,9 +2,11 @@ package com.study.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableZuulProxy
 public class ZuulApplication {
 
